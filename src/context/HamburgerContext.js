f0c39c6d35995766a62e752670eb2468c5a5116f
umit turk <uyt.turk@gmail.com>
2021-04-23 12:16:39 +0300

    main

---
+++ src/context/HamburgerContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from "react";
+
+export const HamburgerContext = createContext();
+
+const HamburgerContextProvider = ( props ) => {
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+
+
+
+  const getCalculatedTotalPrice = () => {
+    return selectedIngredients
+      .reduce((acc, item) => {
+        acc += item.count * item.price;
+        return acc;
+      }, 0)
+      .toFixed(2);
+  };
+  return (
+    <HamburgerContext.Provider
+      value={{
+        selectedIngredients,
+        getCalculatedTotalPrice,
+        setSelectedIngredients/* Component'lere gönderilmesi istenilen state değeri */
+      }}
+    >
+      {props.children}
+    </HamburgerContext.Provider>
+  );
+};
+
+export default HamburgerContextProvider;
